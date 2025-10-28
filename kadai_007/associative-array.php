1140f78cb2df8c308efedb63b091a6cf89adaf50
shinya081877-create <shinya081877@gmail.com>
2025-10-28 14:01:22 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>kadai_007</title>
+</head>
+<body>
+ <p>
+   <?php
+  //  連想配列
+  $personal_data=['name'=>'onion','price'=>'200','weight'=>'160'];
+  print_r($personal_data)
+   ?>
+ </p> 
+</body>
+</html>
